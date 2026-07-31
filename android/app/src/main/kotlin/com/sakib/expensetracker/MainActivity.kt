e0d6c499e41ef76338f4d4c69fe22bb5412338bc
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2026-07-31 23:04:05 +0600

    firebase_added

--- android/app/src/main/kotlin/com/sakib/expensetracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/sakib/expensetracker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pj1
+package com.sakib.expensetracker
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'pages/dashboard_page.dart';
 import 'state/finance_app_state.dart';
 
 // Entry point: Flutter starts running the app from here.
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
